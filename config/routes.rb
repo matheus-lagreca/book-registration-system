f643bbd055542fc7691bf795d9545da5f01204ca
matheus-lagreca <matheus.lagreca@gmail.com>
2022-06-28 18:17:42 -0300

    feat: Login and logout Features

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
 
+  # Session
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # Defines the root path route ("/")
   root to: 'main#index'
 end
