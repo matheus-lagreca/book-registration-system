a9a0b3b01f289805f1c9c15920e72890c9517914
matheus-lagreca <matheus.lagreca@gmail.com>
2022-06-30 13:27:29 -0300

    Add and update migrations

--- db/migrate/20220628223605_create_books.rb
+++ db/migrate/20220628223605_create_books.rb
@@ -1,7 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :author_name
       t.integer :first_publish_year
       t.integer :cover
