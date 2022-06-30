b87ae91847a1c28853945a329e600bf330b66b94
matheus-lagreca <matheus.lagreca@gmail.com>
2022-06-30 13:29:24 -0300

    update models based on last migrations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  belongs_to :user
+
+  validates_presence_of :title, message: 'title is required'
+  validates_presence_of :author_name, message: 'author_name required'
+  validates_presence_of :first_publish_year, message: 'first_publish_year is required'
 end
