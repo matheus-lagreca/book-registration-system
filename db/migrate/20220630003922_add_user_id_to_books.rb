5781da53b21571d5ffebcae5ab7b8938c8253f99
matheus-lagreca <matheus.lagreca@gmail.com>
2022-06-30 13:27:29 -0300

    Add and update migrations

---
+++ db/migrate/20220630003922_add_user_id_to_books.rb
@@ -0,0 +1,4 @@
+class AddUserIdToBooks < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_223605) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_30_003922) do
   create_table "books", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.string "author_name"
     t.integer "first_publish_year"
     t.integer "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_28_223605) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "users"
 end
