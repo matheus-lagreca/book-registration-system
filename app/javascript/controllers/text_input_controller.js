a241ea23e13091edf09560f34009dd8066a5b0e1
matheus-lagreca <matheus.lagreca@gmail.com>
2022-07-01 14:22:49 -0300

    wip: testing stimulus

--- app/javascript/controllers/text_input_controller.js
+++ app/javascript/controllers/text_input_controller.js
@@ -1,9 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
+// html test 
+// <br><br>
+
+// <div data-controller='text-input'>
+//   <input type="text" data-action='text-input#changed' data-text-input-target="input">
+//   <h1 data-text-input-target='input'></h1>  
+// </div>
+
+// <br><br>
+
 export default class extends Controller {
   static targets = ["input", "output"]
 
-  connect() { }
+  connect() {
+  }
 
   changed() {
     this.outputTarget.textContent = this.inputTarget.value
