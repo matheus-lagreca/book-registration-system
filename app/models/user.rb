3d6d85808131770e2c8a74d60208896be344c61a
matheus-lagreca <matheus.lagreca@gmail.com>
2022-07-03 19:51:28 -0300

    feat: email text validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :books
 
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Must be a valid Email address.' }
 end
