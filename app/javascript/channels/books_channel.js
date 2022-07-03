0355a385c5f5564ce4d1646262ed99b5c16ee5d3
matheus-lagreca <matheus.lagreca@gmail.com>
2022-07-03 13:33:46 -0300

    feat: Sockets working when adding a new book

--- app/javascript/channels/books_channel.js
+++ app/javascript/channels/books_channel.js
@@ -13,5 +13,35 @@ consumer.subscriptions.create("BooksChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     console.log(data)
+    const element = document.getElementById('home-page')
+
+    // change this to render partial
+
+    const item = $('<li class="li"/>')
+    const div = $('<div class="single-book"/>').appendTo(item)
+    const body = $('<div class="book-body"/>').appendTo(div)
+
+    // cover
+    $('<img>', {
+      id: 'cover_edition_key_id',
+      src: `https://covers.openlibrary.org/b/olid/${data.book.cover_edition_key}-M.jpg`,
+      class: 'book-image',
+      alt: '/images/textImg.jpg'
+    }).appendTo(div)
+
+    // Text
+    $('<h5 class="book-title"/>').text(`${data.book.title}`).appendTo(body)
+    $(`<small class="book-author">${data.book.author_name[0]}</>`).appendTo(body)
+    $(`<small class="book-first-publish-year">${data.book.first_publish_year}</>`).appendTo(body)
+
+    // buttons?
+
+
+    // prepend to book list in main
+    const allBooks = $("#all-books")
+    allBooks.prepend(item)
+
+
+
   }
 });
--- app/javascript/controllers/bookAPI.js
+++ app/javascript/controllers/bookAPI.js
@@ -25,34 +25,37 @@ window.addEventListener('turbo:load', () => {
 
 
           $.each(searchResults, (i) => {
-            const li = $('<li class="li"/>').appendTo(list);
+            const item = $('<li class="li"/>').appendTo(list);
+            const div = $('<div class="single-book"/>').appendTo(item)
+            const body = $('<div class="book-body"/>').appendTo(div)
+            const bottom = $('<div class="book-register"/>').appendTo(div)
 
-            const div = $('<div class="single-book"/>').appendTo(li)
-
+            // cover
             $('<img>', {
-              id: 'imgId',
+              id: 'cover_id',
               src: `https://covers.openlibrary.org/b/olid/${searchResults[i].cover_edition_key}-M.jpg`,
               class: 'book-image',
               alt: '/images/textImgL.jpg'
             }).appendTo(div)
 
-            $('<h5 class="book-title"/>').text(`${searchResults[i].title}`).appendTo(div)
-            $(`<small class="book-author">${searchResults[i].author_name[0]}</>`).appendTo(div)
-            $(`<small class="book-first-publish-year">${searchResults[i].first_publish_year}</>`).appendTo(div)
+            // Text
+            $('<h5 class="book-title"/>').text(`${searchResults[i].title}`).appendTo(body)
+            $(`<small class="book-author">${searchResults[i].author_name[0]}</>`).appendTo(body)
+            $(`<small class="book-first-publish-year">${searchResults[i].first_publish_year}</>`).appendTo(body)
 
-            $(`<button id="btnSubmit" class="btn btn-primary rounded-pill m-1" onclick=${selectBook(searchResults[i])}> Register Book </>`).appendTo(div)
+            // Button
+            $(`<button id="btnSubmit" class="btn btn-primary rounded-pill m-1" onclick=${selectBook(searchResults[i])}> Register Book </>`).appendTo(bottom)
 
           });
         });
-
       }, 500);
     });
   }
 
   function selectBook(book) {
     $('#book_title').val(book.title)
-    $('#book_author_name').val(book.author_name)
+    $('#book_author_name').val(book.author_name[0])
     $('#book_first_publish_year').val(book.first_publish_year)
-    $('#book_cover_edition_key').val(`https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-M.jpg`)
+    $('#book_cover_edition_key').val(book.cover_edition_key)
   }
 })
